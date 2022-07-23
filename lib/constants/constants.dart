dfbfee12ea09e028c6addd28eda5df20ee4031d0
Rozan <Kids clothing shop>
2022-07-23 14:14:09 +0300

    Meal Monkey delivery App

---
+++ lib/constants/constants.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+final key = GlobalKey<FormState>();
+final nameController = TextEditingController();
+final emailController = TextEditingController();
+final mobileNoController = TextEditingController();
+final addressController = TextEditingController();
+final passwordController = TextEditingController();
+final confPasswordController = TextEditingController();
+final otpController = TextEditingController();
+const Main_Color = Color(0xffFC6011);
